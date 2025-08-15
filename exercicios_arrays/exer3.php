782360d36ac98aead56748d5c15e54ef1bc04c61
AmandaaViana <amandaviana.ahv@gmail.com>
2025-08-15 15:35:45 -0300

    Add files via upload

---
+++ exercicios_arrays/exer3.php
@@ -0,0 +1,22 @@
+<?php
+
+$flor = array ("Orquidea", "Margarida", "Petunia");
+$fruta = array ("Laranja", "maça", "Limao");
+$cidade = array ("Foz do Iguaçu", "Cascavel", "Toledo");
+$turismo = array ("Itaipu", "Cataratas", "Parque das Aves");
+
+$matriz = array ();
+array_push($matriz,$flor,$fruta,$cidade,$turismo);
+
+echo "<table border=1>";
+echo "<tr>";
+echo "<td>Categoria</td>";
+echo "<td>Item</td>";
+echo "</tr>";
+
+
+echo "</table>";
+
+?>
+
+
