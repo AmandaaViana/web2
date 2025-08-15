9aa9bfef39b9838a65f4c221f05c9e64255af1b1
AmandaaViana <amandaviana.ahv@gmail.com>
2025-08-15 14:41:41 -0300

    Update exer2.php

--- exercicios_arrays/exer2.php
+++ exercicios_arrays/exer2.php
@@ -6,6 +6,10 @@ $segundo = array ();
 for($i=0;$i<count($primeiro);$i++){
     array_push($segundo,$primeiro[$i]);
 }
-    print_r($segundo);
-   
+for ($i = 0; $i < count($segundo); $i++) {
+    echo $segundo[$i];
+    if ($i < count($segundo) - 1) { 
+        echo ", ";
+    }
+}  
 ?>
