281eed12dca55cd723817135cb930365612e68fb
AmandaaViana <amandaviana.ahv@gmail.com>
2025-08-15 16:22:49 -0300

    Update exer3.php

--- exercicios_arrays/exer3.php
+++ exercicios_arrays/exer3.php
@@ -8,15 +8,24 @@ $turismo = array ("Itaipu", "Cataratas", "Parque das Aves");
 $matriz = array ();
 array_push($matriz,$flor,$fruta,$cidade,$turismo);
 
-echo "<table border=1>";
-echo "<tr>";
-echo "<td>Categoria</td>";
-echo "<td>Item</td>";
-echo "</tr>";
+$categorias = array("Flores", "Frutas", "Cidades", "Pontos Turísticos");
 
+echo "<table border='1'>";
+echo "<tr><th>Categoria</th><th>Itens</th></tr>";
 
+for ($i = 0; $i < count($matriz); $i++) {
+    echo "<tr>";
+    echo "<td>" . $categorias[$i] . "</td>";
+    echo "<td>";
+  
+for ($j = 0; $j < count($matriz[$i]); $j++) {
+        echo $matriz[$i][$j];
+        if ($j < count($matriz[$i]) - 1) {
+            echo ", ";
+        }
+    }
+    echo "</td>";
+    echo "</tr>";
+}
 echo "</table>";
-
 ?>
-
-
