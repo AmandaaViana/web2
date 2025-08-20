93d80df6410b79f444112d9e1da086f61177428b
AmandaaViana <amandaviana.ahv@gmail.com>
2025-08-20 20:40:53 -0300

    Update pa.php

--- exercicios_aula3/pa.php
+++ exercicios_aula3/pa.php
@@ -1,23 +1,23 @@
 <?php
-//correto so esqueci de fazer a parte do post
-  echo "Parametros recebidos: <br> <br>";
- 
+
   $inicio = $_GET["inicio"];
   $razao = $_GET["razao"];
   $quantidade = $_GET["quantidade"];
  
  if (isset($_GET['inicio']) && isset($_GET['razao']) && isset($_GET['quantidade'])) {
-	echo "Todos os valores foram recebidos";
+	echo "Todos os valores foram recebidos. <br>";
+	echo " A progressão aritmética é: <br> ";
       for ($i = 0; $i < $quantidade; $i++) {
       	$termo = $inicio + ($i * $razao);
-		}
-     echo " A progressão aritmética e: " . $termo . "<br>";
+		echo $termo . "<br>";
+	}
+    
     } else if (isset($_GET['inicio']) && isset($_GET['razao']) && !isset($_GET['quantidade'])) {
-    	echo "Os valores de inicio e razao foram recebidos";
+    	echo "Os valores de inicio e razao foram recebidos.";
 	} else if (isset($_GET['inicio']) && !isset($_GET['razao']) && isset($_GET['quantidade'])) {
-    	echo "Os valores de inicio e quantidade foram recebidos";
+    	echo "Os valores de inicio e quantidade foram recebidos.";
 	} else if (!isset($_GET['inicio']) && isset($_GET['razao']) && isset($_GET['quantidade'])) {
-    	echo "os valores de razao e quantidade foram recebidos";
+    	echo "os valores de razao e quantidade foram recebidos.";
 	} else {
     	echo "Nenhum parâmetro foi recebido";
 }
