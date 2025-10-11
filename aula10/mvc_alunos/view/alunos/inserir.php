1e6762cfa8aba8b5e4d802532fad87134d0ef66f
AmandaaViana <amandaviana.ahv@gmail.com>
2025-10-10 21:55:45 -0300

    Add files via upload

---
+++ aula10/mvc_alunos/view/alunos/inserir.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//verificar se o usuario clicou em gravar
+if(isset($_POST['nome'])){
+    echo "Ja clicou!";
+    //capturar o valores preenchidos no formulario
+    
+}
+
+include_once(__DIR__."/form.php")
+
+?>
---
+++ aula10/mvc_alunos/view/alunos/listar.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once(__DIR__. "/../../controller/AlunoController.php");
+
+$alunoCont = new AlunoController();
+$alunos = $alunoCont->listar();
+//print_r($alunos);
+
+include_once(__DIR__. "/../include/header.php");
+?>
+
+<h3>Listagem de Alunos</h3> 
+
+<div>
+    <a href="inserir.php">Inserir</a>
+</div>
+
+<table>
+    <!-- Cabeçalho -->
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Idade</th>
+        <th>Estrangeiro</th>
+        <th>Curso</th>
+        <th></th>
+        <th></th>
+    </tr>
+
+    <!-- Dados -->
+   
+    <?php foreach($alunos as $a): ?>
+        <tr>
+            <td><?= $a->getId() ?></td>
+            <td><?= $a->getNome() ?></td>
+            <td><?= $a->getIdade() ?></td>
+            <td><?= $a->getiEstrangeiro() ?></td>
+            <td><?= $a->getCurso()->getNomeTurno() ?></td>
+            <td></td>
+            <td></td>
+        </tr>
+    <?php endforeach; ?>
+
+</table>
+
+<?php
+include_once(__DIR__. "/../include/footer.php");
+?>
