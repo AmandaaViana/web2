df1233757dffba7aa4ceb6e8adcfded2392dcc1a
AmandaaViana <amandaviana.ahv@gmail.com>
2025-10-31 21:46:29 -0300

    Add files via upload

---
+++ aula10/mvc_alunos/mvc_alunos/controller/AlunoController.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once(__DIR__. "/../dao/AlunoDao.php");
+include_once(__DIR__. "/../service/AlunoService.php");
+
+class AlunoController{
+
+    private AlunoDao $alunoDao;
+    private AlunoService $alunoServise;
+
+    public function __construct()
+    {
+        $this->alunoDao = new AlunoDao();
+        $this->alunoServise = new AlunoService();
+    }
+
+    public function listar(){
+        return $this->alunoDao->list();
+    }
+
+    public function buscarPorId(int $id){
+        return $this->alunoDao->findById($id);
+    }
+
+    public function inserir(Aluno $aluno){//metodo chama o inserir
+        
+        $erros = $this->alunoServise->validar($aluno);
+        if(! $erros)
+            $this->alunoDao->insert($aluno);
+        return $erros;
+    }
+    public function excluir(int $id) {
+        $this->alunoDao->delete($id);
+    }
+
+
+}
+
+?>
---
+++ aula10/mvc_alunos/mvc_alunos/view/alunos/inserir.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once(__DIR__."/../../model/Aluno.php");
+include_once(__DIR__."/../../controller/AlunoController.php");
+
+$msgErro = "";
+$aluno = NULL;
+
+//verificar se o usuario clicou em gravar
+if(isset($_POST['nome'])){
+    //capturar o valores preenchidos no formulario
+    $nome = trim($_POST['nome']) ? trim($_POST['nome']) : NULL; //operador ternario 
+    $idade = is_numeric($_POST['idade']) ? $_POST['idade'] : NULL;
+    $estrangeiro = trim($_POST['estrang']) ? trim($_POST['estrang']) : NULL;
+    $idCurso = is_numeric($_POST['curso']) ? $_POST['curso'] : NULL;
+
+    //criar objeto aluno
+    $aluno = new Aluno();
+    $aluno->setId(0);
+    $aluno->setNome($nome);
+    $aluno->setIdade($idade);
+    $aluno->setEstrangeiro($estrangeiro);
+
+    if($idCurso){
+        $curso = new Curso();
+        $curso->setId($idCurso);
+        $aluno->setCurso($curso);
+    }else
+        $aluno->setCurso(NULL);
+    //print_r($aluno);
+    $alunoCont = new AlunoController;
+    $erros = $alunoCont->inserir($aluno);
+
+    if(! $erros)
+        header("location: listar.php");
+    else {
+        $msgErro = implode("<br>",$erros);
+    }
+}
+
+include_once(__DIR__."/form.php")
+
+?>
